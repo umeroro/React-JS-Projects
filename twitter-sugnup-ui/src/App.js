ad5427f0a430afb5243fd5344ada83cfeda60427
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-01-24 23:37:18 +0500

    Twitter SignUp Page

--- twitter-sugnup-ui/src/App.js
+++ twitter-sugnup-ui/src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className='container'>
+        <div className='twitterSignup-Box'>
+          <img src='./twitter-icon.svg' alt='twitter-logo' />
+          <h2>Sign in to Twitter</h2>
+          <div>
+            <button className='SignBtn'><img src='./google-icon.svg' alt='google-logo'/>Sign in with Google</button>
+          </div>
+          <div>
+            <button className='SignBtn'><img src='./apple-icon.svg' alt='google-logo'/>Sign in with Apple</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
