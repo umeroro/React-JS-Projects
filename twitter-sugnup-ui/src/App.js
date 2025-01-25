5f674d77111fbad01bc197052e83ee8f776ab9da
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-01-25 17:04:54 +0500

    Twitter Signup Page

--- twitter-sugnup-ui/src/App.js
+++ twitter-sugnup-ui/src/App.js
@@ -1,18 +1,47 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className='container'>
-        <div className='twitterSignup-Box'>
-          <img src='./twitter-icon.svg' alt='twitter-logo' />
+      <div className="container">
+        <div className="twitterSignup-Box">
+          <img src="./twitter-icon.svg" alt="twitter-logo" className="logo" />
           <h2>Sign in to Twitter</h2>
-          <div>
-            <button className='SignBtn'><img src='./google-icon.svg' alt='google-logo'/>Sign in with Google</button>
+          <div className="buttonsDiv">
+            <button className="SignBtn">
+              <img src="./google-icon.svg" alt="google-logo" />
+              Sign in with Google
+            </button>
           </div>
-          <div>
-            <button className='SignBtn'><img src='./apple-icon.svg' alt='google-logo'/>Sign in with Apple</button>
+          <div className="buttonsDiv">
+            <button className="SignBtn">
+              <img src="./apple-icon.svg" alt="google-logo" />
+              Sign in with Apple
+            </button>
           </div>
+          <div className="and">
+            <div>
+              <div className="divider"></div>
+            </div>
+            <div className="or">or</div>
+            <div>
+              <div className="divider"></div>
+            </div>
+          </div>
+          <input
+            type="text"
+            placeholder="Phone, email address, or username"
+            className="inputtext"
+          />
+          <div className="buttonsDiv">
+            <button className="SignBtn nextBtn">Next</button>
+          </div>
+          <div className="buttonsDiv">
+            <button className="SignBtn forgetBtn">Forget password?</button>
+          </div>
+          <p className="acountConf">
+            Don’t have an account? <a href="localhost:3000">Sign up</a>
+          </p>
         </div>
       </div>
     </div>
