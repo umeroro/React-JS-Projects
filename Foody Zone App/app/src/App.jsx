36c887197e469bd956413640363efd438d6a187a
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-02-16 15:28:17 +0500

    Foody Zone App

--- Foody Zone App/app/src/App.jsx
+++ Foody Zone App/app/src/App.jsx
@@ -1,5 +1,48 @@
+import styled from "styled-components";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Maincontainer>
+      <Topcontainer>
+        <div className="logo">
+          <img src="logo.svg" alt="site logo" />
+        </div>
+        <div className="search">
+          <input type="text" placeholder="Search Food..." />
+        </div>
+      </Topcontainer>
+    </Maincontainer>
+  );
 };
 
 export default App;
+
+const Maincontainer = styled.div`
+  background-color: #323334;
+  max-width: 1260px;
+  margin: 0 auto;
+`;
+const Topcontainer = styled.div`
+  display: flex;
+  padding: 80px 20px 40px;
+  justify-content: space-between;
+  align-items: center;
+
+  .search input[type="text"] {
+    border: 2px solid red;
+    background-color: transparent;
+    padding: 10px;
+    border-radius: 5px;
+  }
+  .search input[type="text"]::placeholder {
+    color: white;
+    font-size: 16px;
+    font-weight: 400;
+  }
+  .search input[type="text"]:focus {
+    color: white;
+    font-size: 16px;
+    font-weight: 400;
+    outline: none;
+  }
+`;
--- Foody Zone App/app/src/main.jsx
+++ Foody Zone App/app/src/main.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { createGlobalStyle } from "styled-components";
+
+const Globalstyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  body {
+    background-color: #323334;
+    color: #fff;
+  }
+`;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Globalstyle />
     <App />
   </React.StrictMode>
 );
