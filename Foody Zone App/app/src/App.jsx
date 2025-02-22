dc373c19d4bb0e59c220c020b90a0822f737a1b1
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-02-22 19:29:04 +0500

    Foody Zone App Completed

--- Foody Zone App/app/src/App.jsx
+++ Foody Zone App/app/src/App.jsx
@@ -1,80 +1,166 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import SearchResult from "./components/SearchResults/SearchResult";
+
+export const BASE_URL = "http://localhost:9000";
 
 const App = () => {
+  const [data, setData] = useState(null);
+  const [filteredData, setFilteredData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [selectedBtn, setSelectedBtn] = useState("all");
+
+  useEffect(() => {
+    const fetchFoodData = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch(BASE_URL);
+
+        const json = await response.json();
+
+        setData(json);
+        setFilteredData(json);
+        setLoading(false);
+      } catch (error) {
+        setError("Unable to fetch data");
+      }
+    };
+    fetchFoodData();
+  }, []);
+
+  const searchFood = (e) => {
+    const searchValue = e.target.value;
+
+    console.log(searchValue);
+
+    if (searchValue === "") {
+      setFilteredData(null);
+    }
+
+    const filter = data?.filter((food) =>
+      food.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilteredData(filter);
+  };
+
+  const filterFood = (type) => {
+    if (type === "all") {
+      setFilteredData(data);
+      setSelectedBtn("all");
+      return;
+    }
+
+    const filter = data?.filter((food) =>
+      food.type.toLowerCase().includes(type.toLowerCase())
+    );
+    setFilteredData(filter);
+    setSelectedBtn(type);
+  };
+
+  const filterBtns = [
+    {
+      name: "All",
+      type: "all",
+    },
+    {
+      name: "Breakfast",
+      type: "breakfast",
+    },
+    {
+      name: "Lunch",
+      type: "lunch",
+    },
+    {
+      name: "Dinner",
+      type: "dinner",
+    },
+  ];
+
+  if (error) return <div>{error}</div>;
+  if (loading) return <div>loading.....</div>;
+
   return (
-    <Maincontainer>
-      <Topcontainer>
-        <div className="logo">
-          <img src="logo.svg" alt="site logo" />
-        </div>
-        <div className="search">
-          <input type="text" placeholder="Search Food..." />
-        </div>
-      </Topcontainer>
-      <Filtercontainer>
-        <Button>All</Button>
-        <Button>Breakfast</Button>
-        <Button>Lunch</Button>
-        <Button>Dinner</Button>
-      </Filtercontainer>
-      <Foodcardcontainer>
-        <FoodCards>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </FoodCards>
-      </Foodcardcontainer>
-    </Maincontainer>
+    <>
+      <Container>
+        <TopContainer>
+          <div className="logo">
+            <img src="/logo.svg" alt="logo" />
+          </div>
+
+          <div className="search">
+            <input onChange={searchFood} placeholder="Search Food" />
+          </div>
+        </TopContainer>
+
+        <FilterContainer>
+          {filterBtns.map((value) => (
+            <Button
+              isSelected={selectedBtn === value.type}
+              key={value.name}
+              onClick={() => filterFood(value.type)}
+            >
+              {value.name}
+            </Button>
+          ))}
+        </FilterContainer>
+      </Container>
+      <SearchResult data={filteredData} />
+    </>
   );
 };
 
 export default App;
 
-const Maincontainer = styled.div`
+export const Container = styled.div`
   max-width: 1200px;
   margin: 0 auto;
 `;
-const Topcontainer = styled.div`
+const TopContainer = styled.section`
+  height: 140px;
   display: flex;
-  padding: 80px 20px 40px;
   justify-content: space-between;
+  padding: 16px;
   align-items: center;
 
   .search {
-    input{
+    input {
       background-color: transparent;
-      border: 2px solid red;
+      border: 1px solid red;
+      color: white;
       border-radius: 5px;
-      color: white;
+      height: 40px;
       font-size: 16px;
-      font-weight: 400;
-      padding: 10px;
-    }
-    input::placeholder{
-      color: white;
+      padding: 0 10px;
+      &::placeholder {
+        color: white;
+      }
     }
   }
+
+  @media (0 < width < 600px) {
+    flex-direction: column;
+    height: 120px;
+  }
 `;
-const Filtercontainer = styled.section`
+
+const FilterContainer = styled.section`
   display: flex;
   justify-content: center;
-  gap: 20px;
+  gap: 12px;
   padding-bottom: 40px;
 `;
-const Button = styled.button`
-  background-color: #ff4343;
+
+export const Button = styled.button`
+  background: ${({ isSelected }) => (isSelected ? "#f22f2f" : "#ff4343")};
+  outline: 1px solid ${({ isSelected }) => (isSelected ? "white" : "#ff4343")};
   border-radius: 5px;
   padding: 6px 12px;
-  color: white;
   border: none;
+  color: white;
   cursor: pointer;
+  &:hover {
+    background-color: #f22f2f;
+  }
 `;
-const Foodcardcontainer = styled.section`
-  min-height: 69.5vh;
-  background-image: url("bg.png");
-  background-repeat: no-repeat;
-  background-size: cover;
-`;
-const FoodCards = styled.div``;
--- Foody Zone App/app/src/main.jsx
+++ Foody Zone App/app/src/main.jsx
@@ -3,20 +3,26 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createGlobalStyle } from "styled-components";
 
-const Globalstyle = createGlobalStyle`
-  * {
-    padding: 0;
+const GlobalStyle = createGlobalStyle`
+  
+
+  *{
+    box-sizing: border-box;
     margin: 0;
+    padding:0;
   }
-  body {
+  body{
     background-color: #323334;
-    color: #fff;
+    color:white;
+    min-height: 100vh;
+    font-family:  'Inter', sans-serif;
   }
+
 `;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Globalstyle />
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
