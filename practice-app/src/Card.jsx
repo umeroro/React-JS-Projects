a1d09cd9a5daa678df6859151e6a65ed2aca5bfa
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-02-28 23:01:08 +0500

    Conditional Rendering and State Hook

--- practice-app/src/Card.jsx
+++ practice-app/src/Card.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Card = (props) => {
+
+  const [count, setCount] = useState(0);
+
   const handleClick = () => {
     alert(props.title || "No Title");
   };
 
+  const handleCount = () => {
+    setCount(count + 1);
+  }
+
   return (
     <div style={{ border: "1px solid black", padding: "10px", margin: "10px" }}>
-      <h1>{props.title || "No Title"}</h1>
-      <button onClick={handleClick}>Show Title</button>
-      <p>{props.description || "No Description"}</p>
-      <div></div>
+      {props.title ? (
+        <>
+          <h1>{props.title || "No Title"}</h1>
+          <button onClick={handleClick}>Show Title</button>
+          <p>{props.description || "No Description"}</p>
+        </>
+      ) : (
+        <>
+          <p>No title available</p>
+        </>
+      )}
+      <button onClick={handleCount}>Clicked Me</button>
+      <p>Clicked : {count}</p>  
     </div>
   );
 };
