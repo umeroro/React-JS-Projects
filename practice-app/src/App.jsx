a9196b7861a3ce785771fb01d847e7c26287b92f
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-02-28 23:01:15 +0500

    Conditional Rendering and State Hook

--- practice-app/src/App.jsx
+++ practice-app/src/App.jsx
@@ -2,25 +2,24 @@ import React from "react";
 import Card from "./Card";
 
 function App() {
-  // const cardData = [
-  //   { title: "Card 1", description: "This is 1st Description" },
-  //   { title: "Card 2", description: "This is 2nd Description" },
-  //   { title: "Card 3", description: "This is 3rd Description" },
-  // ];
+  const cardData = [
+    { title: "Card 1", description: "This is 1st Description" },
+    { title: "Card 2", description: "This is 2nd Description" },
+    { title: "Card 3", description: "This is 3rd Description" },
+  ];
 
   const title = true;
 
   return (
     <>
       <div>
-        {/* {cardData.map((card, index) => (
+        {cardData.map((card, index) => (
           <Card
             key={index}
             title={card.title}
             description={card.description}
           />
-        ))} */}
-        <Card {title ?}/>
+        ))}
       </div>
     </>
   );
