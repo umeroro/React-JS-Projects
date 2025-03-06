20d8cc324bea05e90e9348bd3b324c7641e89357
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-03-06 21:27:15 +0500

    practice-app

--- practice-app/src/App.jsx
+++ practice-app/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     { title: "Card 2", description: "This is 2nd Description" },
     { title: "Card 3", description: "This is 3rd Description" },
     { title: "Card 3", description: "This is 3rd Description" },
+    { title: "Card 3", description: "This is 3rd Description" },
   ];
 
   const title = true;
