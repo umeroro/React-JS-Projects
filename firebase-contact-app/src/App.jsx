eba727a238560f44a364885dd94bfd1682c4ecc5
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-05-11 15:09:41 +0500

    Firebase Contact App

--- firebase-contact-app/src/App.jsx
+++ firebase-contact-app/src/App.jsx
@@ -5,9 +5,19 @@ import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./config/firebase";
 import ContactCard from "./components/ContactCard";
+import Modal from "./components/Modal";
 
 const App = () => {
   const [contacts, setContact] = useState([]);
+  const [isOpen, setOpen] = useState(false);
+
+  const onOpen = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     const getContact = async () => {
@@ -29,25 +39,28 @@ const App = () => {
   }, []);
 
   return (
-    <div className="mx-auto max-w-[370px] p-4">
-      <Navbar />
-      <div className="flex items-center gap-2">
-        <div className="relative flex flex-grow items-center">
-          <FiSearch className="text-white text-2xl absolute ml-1" />
-          <input
-            type="text"
-            placeholder="Search Contact"
-            className="h-10 flex-grow rounded-md border border-white bg-transparent text-white pl-9"
-          />
+    <>
+      <div className="mx-auto max-w-[370px] p-4">
+        <Navbar />
+        <div className="flex items-center gap-2">
+          <div className="relative flex flex-grow items-center">
+            <FiSearch className="text-white text-2xl absolute ml-1" />
+            <input
+              type="text"
+              placeholder="Search Contact"
+              className="h-10 flex-grow rounded-md border border-white bg-transparent text-white pl-9"
+            />
+          </div>
+          <AiFillPlusCircle onClick={onOpen} className="text-4xl text-white cursor-pointer" />
         </div>
-        <AiFillPlusCircle className="text-4xl text-white cursor-pointer" />
+        <div className="pt-[20px] flex flex-col gap-3">
+          {contacts.map((contact) => (
+            <ContactCard key={contact.id} contact={contact} />
+          ))}
+        </div>
       </div>
-      <div className="pt-[20px]">
-        {contacts.map((contact) => (
-          <ContactCard key={contact.id} contact={contact} />
-        ))}
-      </div>
-    </div>
+      <Modal isOpen={isOpen} onClose={onClose}/>
+    </>
   );
 };
 
