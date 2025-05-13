ca4c8582c9e40e1af4a005bf541a6b211702007b
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-05-13 22:42:21 +0500

    Firebase Contact App

---
+++ firebase-contact-app/src/components/AddAndUpdate.jsx
@@ -0,0 +1,11 @@
+import Modal from "./Modal";
+
+const AddAndUpdate = ({isOpen,onClose}) => {
+  return (
+    <div>
+      <Modal isOpen={isOpen} onClose={onClose}>HI</Modal>
+    </div>
+  );
+};
+
+export default AddAndUpdate;
--- firebase-contact-app/src/components/Modal.jsx
+++ firebase-contact-app/src/components/Modal.jsx
@@ -1,25 +1,28 @@
+import { createPortal } from "react-dom";
 import { AiOutlineClose } from "react-icons/ai";
 
 const Modal = ({ onClose, isOpen, children }) => {
-  return (
+  return createPortal(
     <>
       {isOpen && (
         <>
-          <div className="min-h-[200px] max-w-[80%] m-auto z-40 relative bg-[white] rounded-[8px] p-4">
+          <div className="min-h-[200px] max-w-[80%] m-auto z-50 relative bg-[white] rounded-[8px] p-4">
             <div className="flex justify-end">
               <AiOutlineClose
                 onClick={onClose}
                 className="cursor-pointer text-xl self-end"
               />
             </div>
+            {children}
           </div>
           <div
             onClick={onClose}
-            className="backdrop-blur h-screen w-screen absolute top-0 z-50"
+            className="backdrop-blur h-screen w-screen absolute top-0 z-40"
           ></div>
         </>
       )}
-    </>
+    </>,
+    document.getElementById("model-root")
   );
 };
 
