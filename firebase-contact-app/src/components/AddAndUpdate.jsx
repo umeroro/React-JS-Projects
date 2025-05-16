81b775970d45149223a7d1ea30ae4ac2676dfad4
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-05-16 11:03:50 +0500

    Firebase Contact App

--- firebase-contact-app/src/components/AddAndUpdate.jsx
+++ firebase-contact-app/src/components/AddAndUpdate.jsx
@@ -1,9 +1,23 @@
+import { Field, Form, Formik } from "formik";
 import Modal from "./Modal";
 
-const AddAndUpdate = ({isOpen,onClose}) => {
+const AddAndUpdate = ({ isOpen, onClose }) => {
   return (
     <div>
-      <Modal isOpen={isOpen} onClose={onClose}>HI</Modal>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <Formik>
+          <Form>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="name">Name: </label>
+              <Field name="name" className="border h-7" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="email">Email: </label>
+              <Field name="email" className="border h-7" />
+            </div>
+          </Form>
+        </Formik>
+      </Modal>
     </div>
   );
 };
